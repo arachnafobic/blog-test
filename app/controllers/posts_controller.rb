1d625e80e2b7a819ac8538c6e6bcd76c1fee0d49
Remco van Hest <remco@arachnafobic.org>
2013-11-24 22:16:10 +0100

    setup posts controller and created basic post functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # In Rails 4.x it should be like this:
+    # @post = Post.new(params[:post].permit(:title, :text))
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    # In Rails 4.x it should be like this:
+    # if @post.update(params[:post].permit(:title, :text))
+    if @post.update_attributes(params[:post])
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+end
