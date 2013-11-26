7783f6771ecafea87254d3e672f927862c4165d2
Remco van Hest <remco@arachnafobic.org>
2013-11-26 22:30:35 +0100

    destroying posts now also removes linked slugs

--- app/models/post_observer.rb
+++ app/models/post_observer.rb
@@ -11,9 +11,9 @@ class PostObserver < ActiveRecord::Observer
     Slug[post.slug] = post.id.to_s
     return true
   end
-
   def after_destroy(post)
-    Slug.destroy(post.id.to_s)
+#    Slug.destroy(post.id.to_s)
+    Slug.destroy(Slug[post.slug])
     return true
   end
 end
